7e8cb821f8806a817ce4429328fce6b0f1c6c50f
ayushigupta931 <ayushigupta931@gmail.com>
2022-01-06 20:15:27 +0530

    Initial commit

---
+++ app/src/main/java/com/android/example/messapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.example.messapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
