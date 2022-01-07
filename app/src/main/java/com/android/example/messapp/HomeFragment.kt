92fe79e7f169f35dd58d62010edc3ddd4f8e62b4
mihir02badkur <mihir27badkur@gmail.com>
2022-01-07 22:43:15 +0530

    action bar resolved

--- app/src/main/java/com/android/example/messapp/HomeFragment.kt
+++ app/src/main/java/com/android/example/messapp/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.android.example.messapp.databinding.FragmentHomeBinding
 import com.android.example.messapp.databinding.FragmentLoginBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -24,7 +25,8 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
         val tabLayout = binding.tabLayout
         val viewPager = binding.daysViewPager
         val list = resources.getStringArray(R.array.days)
--- app/src/main/java/com/android/example/messapp/MainActivity.kt
+++ app/src/main/java/com/android/example/messapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+
 
     }
 }
