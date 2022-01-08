ed2a0d6804dbfb0db5bc008a8771020f99637a5d
mihir02badkur <mihir27badkur@gmail.com>
2022-01-08 16:16:25 +0530

    history adapter create

---
+++ app/src/main/java/com/android/example/messapp/HistoryAdapter.kt
@@ -0,0 +1,61 @@
+package com.android.example.messapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.android.example.messapp.databinding.HistoryCardviewBinding
+
+class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.ViewHolder>(){
+    private var dates:List<Date>  = emptyList<Date>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ):HistoryAdapter.ViewHolder {
+        val binding = HistoryCardviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HistoryAdapter.ViewHolder, position: Int) {
+        with(holder){
+            with(dates[position]){
+                binding.date.text = this.date
+                if(this.breakfast){
+                    binding.breakfast.text="Breakfast: Done"
+                }else{
+                    binding.breakfast.text="Breakfast: Cancelled"
+                }
+
+                if(this.lunch){
+                    binding.lunch.text="Lunch: Done"
+                }else{
+                    binding.lunch.text="Lunch: Cancelled"
+                }
+
+                if(this.dinner){
+                    binding.dinner.text="Dinner: Done"
+                }else{
+                    binding.dinner.text="Dinner: Cancelled"
+                }
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return dates.size
+    }
+    fun setBirthdays(birthdays: List<Date>){
+        this.dates = birthdays
+        notifyDataSetChanged()
+    }
+    inner class ViewHolder(val binding: HistoryCardviewBinding): RecyclerView.ViewHolder(binding.root){
+
+
+    }
+
+
+}
--- app/src/main/java/com/android/example/messapp/HistoryFragment.kt
+++ app/src/main/java/com/android/example/messapp/HistoryFragment.kt
@@ -5,17 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import com.android.example.messapp.databinding.FragmentHistoryBinding
+import com.android.example.messapp.databinding.FragmentLoginBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class HistoryFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentHistoryBinding
+    private val viewModel by viewModels<UserViewmodel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+        (activity as AppCompatActivity).supportActionBar?.title = "History"
+    }
 }
