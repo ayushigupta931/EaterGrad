cebeeea1ee189f85a817398aed99c704fb7e21bc
RainaJain5 <rj35100@gmail.com>
2022-01-09 14:03:09 +0530

    Expense fixed

--- app/src/main/java/com/android/example/messapp/ExpensesFragment.kt
+++ app/src/main/java/com/android/example/messapp/ExpensesFragment.kt
@@ -7,71 +7,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.android.example.messapp.databinding.FragmentExpensesBinding
 import com.android.example.messapp.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class ExpensesFragment : Fragment() {
 
     private lateinit var binding: FragmentExpensesBinding
     private val db = Firebase.firestore
-    var brkFastCnt: Int= 30
-    var lunchCnt: Int=30
-    var dinnerCnt: Int=30
+    private val auth: FirebaseAuth = Firebase.auth
+    private val uid = auth.currentUser.toString()
+    private val docref= db.collection("user"). document(uid). collection ("date")
+    /* Breakfast Expense = 25
+       Lunch Expense = 40
+       Dinner Expense = 45
+     */
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var brkFastCnt = 30
+        var lunchCnt = 30
+        var dinnerCnt = 30
 
-        val auth: FirebaseAuth = Firebase.auth
-        val uid = auth.currentUser.toString()
-       val docref= db.collection("user"). document(uid). collection ("date")
+        lifecycleScope.launchWhenCreated {
+            brkFastCnt -= mealFalse("breakfast")
+            lunchCnt -= mealFalse("lunch")
+            dinnerCnt -= mealFalse("dinner")
+        }
 
+        brkFastCnt *= 25
+        lunchCnt *= 40
+        dinnerCnt *= 45
+        val total = brkFastCnt+lunchCnt+dinnerCnt
 
-
-        docref.whereEqualTo("breakfast", false)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    brkFastCnt=brkFastCnt-1
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
-        docref.whereEqualTo("lunch", false)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    lunchCnt=lunchCnt-1
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
-        docref.whereEqualTo("dinner", false)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    dinnerCnt=dinnerCnt-1
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
         binding = FragmentExpensesBinding.inflate(inflater,container,false)
-        binding.BfastCnt.text = brkFastCnt.toString()
-        binding.LunchCnt.text = lunchCnt.toString()
-        binding.DinnerCnt.text = dinnerCnt.toString()
-        return binding.root
 
+        binding.BfastCnt.text = "₹ " + brkFastCnt.toString()
+        binding.LunchCnt.text = "₹ " + lunchCnt.toString()
+        binding.DinnerCnt.text = "₹ " + dinnerCnt.toString()
+        binding.MthExp.text = "₹ " + total.toString()
+
+        return binding.root
     }
 
+    private suspend fun mealFalse(meal : String):Int {
+        var count=0
+
+        return suspendCoroutine {
+            docref.whereEqualTo(meal, false)
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        count++
+                    }
+                    it.resume(count)
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents: ", exception)
+                }
+            count
+        }
+    }
 
   }
